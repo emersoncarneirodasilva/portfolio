19b9cc44ae5db93ca0b95600e74b84c0473a233e
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-02-22 21:04:43 -0300

    up

---
+++ js/style-switcher.js
@@ -0,0 +1,42 @@
+/* ========================= Toggle Style Switcher ========================= */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler")
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open")
+})
+
+// Hide style - switcher on scroll //
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open")
+    }
+})
+
+/* ========================= Theme Colors ========================= */
+const alternateStyles = document.querySelectorAll(".alternate-style")
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")) {
+            style.removeAttribute("disabled")
+        }
+        else {
+            style.setAttribute("disabled", "true")
+        }
+    })
+}
+
+/* ========================= Theme Light and Dark Mode ========================= */
+const dayNight = document.querySelector(".day-night")
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("em").classList.toggle("fa-sun")
+    dayNight.querySelector("em").classList.toggle("fa-moon")
+    document.body.classList.toggle("dark")
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark")) {
+        dayNight.querySelector("em").classList.add("fa-sun")
+    }
+    else {
+        dayNight.querySelector("em").classList.add("fa-moon")
+    }
+})
